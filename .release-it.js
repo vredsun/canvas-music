e2b7c9a1571974cd45a817570911309e844b9976
vredsun <vokinda@yandex.ru>
2020-03-15 11:20:00 +0300

    test release-it

---
+++ .release-it.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "git": {
+    "tagName": "v${version}"
+  },
+  "npm": {
+    "publish": false,
+  },
+  "github": {
+    "release": true,
+    "tokenRef": "GITHUB_TOKEN"
+  }
+};
