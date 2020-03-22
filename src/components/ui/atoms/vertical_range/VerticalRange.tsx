e38c22e951783d5136c07f801c3f74ccc58ee46a
vredsun <vokinda@yandex.ru>
2020-03-22 14:38:23 +0300

    Closes #33

--- src/components/ui/atoms/vertical_range/VerticalRange.tsx
+++ src/components/ui/atoms/vertical_range/VerticalRange.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import { isNullOrUndefined } from 'util';
 
 type Props = {
   label?: React.ReactNode;
@@ -67,10 +68,11 @@ const calcNewValue = (min: number, max: number, refWithMaxHeight: React.MutableR
 
 const VerticalRange: React.FC<Props> = React.memo(
   (props) => {
+    const [beforeMuteValue, setBeforeMuteValue] = React.useState<number>(null);
     const [isMouseDown, setIsMouseDown] = React.useState(false);
+    const node = React.useRef<HTMLDivElement>();
     const verticalRangeRef = React.useRef<HTMLDivElement>();
 
-    const node = React.useRef<HTMLDivElement>();
     const handleClick = React.useCallback(
       (event: React.MouseEvent<HTMLDivElement, MouseEvent> | React.TouchEvent<HTMLDivElement>) => {
         setIsMouseDown(true);
@@ -153,9 +155,23 @@ const VerticalRange: React.FC<Props> = React.memo(
       [props.value, props.max, props.min],
     );
 
+    const handleMute = React.useCallback(
+      () => {
+        if (props.value) {
+          setBeforeMuteValue(props.value);
+          props.onChange(props.min);
+        } else if (!isNullOrUndefined(beforeMuteValue)) {
+          props.onChange(beforeMuteValue);
+        }
+      },
+      [props.onChange, props.value, beforeMuteValue],
+    );
+
     return (
       <Container>
-        {props.label}
+        <span onClick={handleMute}>
+          {props.label}
+        </span>
         <VerticalRangeBlockContainer
           ref={node}
           onTouchStart={handleClick}
